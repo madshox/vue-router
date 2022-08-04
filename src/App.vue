11572b3619847b05c694c59787dd4e545d96ab6f
madshox <63535913+madshox@users.noreply.github.com>
2022-08-04 11:06:16 +0500

    router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrapper">
+      <header>
+        <div class="navbar">
+          <div class="container">
+            <div class="navbar-content">
+              <div class="logo">VUE-CLI</div>
+              <ul class="navbar-list">
+                <li class="navbar-item">
+                  <a href="" class="navbar-link">Example</a>
+                </li>
+                <li class="navbar-item">
+                  <a href="" class="navbar-link">Home</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <router-view></router-view>
+
+      <!-- <footer></footer> -->
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/pages/Example.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper-content">
+    <section>
+      <div class="container">
+        <h1>Example page</h1>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Example'
+}
+</script>
