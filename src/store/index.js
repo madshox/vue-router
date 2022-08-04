9e766ae73b054220a32d4291e1b77d7b01e20253
madshox <63535913+madshox@users.noreply.github.com>
2022-08-04 14:05:44 +0500

    feat: second initials Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,26 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {},
-    getters: {},
+    state: {
+        message: 'hello vuex'
+    },
+    mutations: {
+        //change state
+        setMessage (state, payload) {
+            state.message = payload;
+        }
+    },
+    actions: {
+        // change state throught mutations for async
+        setMessage ({commit, getters}) {
+            let a = getters.getMessage;
+            let mes = 'API';
+            commit('setMessage', mes)
+        }
+    },
+    getters: {
+        getMessage(state) {
+            return state.message
+        }
+    },
 })
